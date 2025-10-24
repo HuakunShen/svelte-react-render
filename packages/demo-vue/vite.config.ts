16cdb222a3d112d8cbaf9db041e3ccf722353256
Huakun Shen <huakun.shen@huakunshen.com>
2025-10-25 04:37:37 +0800

    add vue shadcn

--- packages/demo-vue/vite.config.ts
+++ packages/demo-vue/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -9,6 +9,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     vueJsx(),
     vueDevTools(),
   ],
